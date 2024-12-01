06c1ea10b65a0162b0f77954bf3524eef743cebd
maxwell0207 <146640866+maxwell0207@users.noreply.github.com>
2024-12-01 17:49:32 -0300

    Atualiza App.js para usar Material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
 import ListarProdutos from './pages/ListarProdutos';
 import AdicionarProduto from './pages/AdicionarProduto';
 import EditarProduto from './pages/EditarProduto';
@@ -7,14 +8,26 @@ import EditarProduto from './pages/EditarProduto';
 function App() {
   return (
     <Router>
-      <nav>
-        <Link to="/">Listar Produtos</Link> | <Link to="/adicionar">Adicionar Produto</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<ListarProdutos />} />
-        <Route path="/adicionar" element={<AdicionarProduto />} />
-        <Route path="/editar/:id" element={<EditarProduto />} />
-      </Routes>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Catálogo de Produtos
+          </Typography>
+          <Button color="inherit" component={Link} to="/">
+            Listar Produtos
+          </Button>
+          <Button color="inherit" component={Link} to="/adicionar">
+            Adicionar Produto
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Container sx={{ marginTop: 4 }}>
+        <Routes>
+          <Route path="/" element={<ListarProdutos />} />
+          <Route path="/adicionar" element={<AdicionarProduto />} />
+          <Route path="/editar/:id" element={<EditarProduto />} />
+        </Routes>
+      </Container>
     </Router>
   );
 }
